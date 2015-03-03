403f1aaaa6f8b57382714cd04c131fbf4768c7bc
Rommel Rico <rommeltj@gmail.com>
2015-03-03 13:50:44 -0800

    Initial Commit.

---
+++ MemorablePlaces/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemorablePlaces
+//
+//  Created by Rommel Rico on 3/3/15.
+//  Copyright (c) 2015 Rommel Rico. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,20 @@
+# Udemy Tutorial 10
+
+Version: 0.0.1 - 03 Mar 2015
+
+## Description
+
+Udemy iOS/Swift tutorials.
+
+### Documentation
+Branches:  
+master = Production branch  
+dev = Development branch  
+
+## Contact
+
+<me@rommelrico.com>
+
+## Notes
+
+If you want to contribute, email me at <me@rommelrico.com>.
