c7f0a56b3089ca6bceaed6e443900bb18a3a0d4e
Rommel Rico <rommeltj@gmail.com>
2015-03-04 09:31:46 -0800

    Starting the app from other codebase
    Pasting most of this code from my “LearningMaps” repository.

--- MemorablePlaces/ViewController.swift
+++ MemorablePlaces/ViewController.swift
@@ -7,12 +7,88 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    var manager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Setting up CoreLocation
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        
+        //Initialize default location.
+        var latitude:CLLocationDegrees = 32.771764
+        var longitude:CLLocationDegrees = -117.189978
+        
+        //Initialize zoom-level
+        var latDelta:CLLocationDegrees = 0.01
+        var lonDelta:CLLocationDegrees = 0.01
+        
+        //Define view variables
+        var span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
+        var location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        var region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        
+        //Set the map to the region
+        mapView.setRegion(region, animated: true)
+        
+        //Set up an initial annotation.
+        var annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        annotation.title = "Spanish Inquisition"
+        annotation.subtitle = "Nobody expects it!"
+        
+        //Add annotation to map.
+        mapView.addAnnotation(annotation)
+        
+        //Allow user to add annotations via long-press
+        var uilpgr = UILongPressGestureRecognizer(target: self, action: "action:")
+        uilpgr.minimumPressDuration = 1.0
+        mapView.addGestureRecognizer(uilpgr)
+        
+    }
+    
+    func action(gestureRecognizer:UIGestureRecognizer) {
+        if (gestureRecognizer.state == UIGestureRecognizerState.Began) {
+            var touchPoint = gestureRecognizer.locationInView(self.mapView)
+            var newCoordinate:CLLocationCoordinate2D = mapView.convertPoint(touchPoint, toCoordinateFromView: self.mapView)
+            var annotation = MKPointAnnotation()
+            annotation.coordinate = newCoordinate
+            annotation.title = "Ni!"
+            mapView.addAnnotation(annotation)
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        var userLocation:CLLocation = locations[0] as CLLocation
+        
+        //Define location
+        var latitude:CLLocationDegrees = userLocation.coordinate.latitude
+        var longitude:CLLocationDegrees = userLocation.coordinate.longitude
+        
+        //Define zoom-level
+        var latDelta:CLLocationDegrees = 0.01
+        var lonDelta:CLLocationDegrees = 0.01
+        
+        //Define view variables
+        var span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
+        var location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        var region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        
+        //Set the map to the region
+        mapView.setRegion(region, animated: true)
+    }
+    
+    func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!) {
+        NSLog("ERROR: \(error)")
     }
 
     override func didReceiveMemoryWarning() {
